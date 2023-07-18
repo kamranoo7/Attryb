2e0a1d1fb909f9a2bae6f37ed378be8ed2d4667b
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-07-18 16:24:20 +0530

    Create_folder

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 let express=require("express")
+const { connection } = require("./db")
 let app=express()
+require("dotenv").config()
 
 
 
 
-
-
-app.listen(8080,()=>{
+//Server connection
+app.listen(process.env.port,async()=>{
+    try{
+await connection
+console.log("Connected to DB")
+    }catch{
+        console.log(err)
+        console.log("Not Connected To DB")
+    }
     console.log("Server is running")
 })
